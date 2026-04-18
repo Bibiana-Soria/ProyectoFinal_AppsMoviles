21f89e220450c19a10d082ad38c91ff0115b6997
Bibiana Soria <bibiana_3141240181@utd.edu.mx>
2026-04-18 16:12:07 -0600

    pantalla 2

--- src/screens/EquipoTrabajo.js
+++ src/screens/EquipoTrabajo.js
@@ -5,16 +5,16 @@ rol de cada integrante
 foto o avatar de cada miembro (si quires o no)
 */
 import React from "react";
-import { View, Text , StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text , StyleSheet, TouchableOpacity, Image, ScrollView } from "react-native";
 
 export default function EquipoTrabajo() {
     return (
-        <View style = {styles.container}>
-            <Text style = {styles.titulo}>Holaa somos dos nomas</Text>
-            <TouchableOpacity style = {styles.boton}>
-                <Text style = {styles.texto_boton}>Helow</Text>
-            </TouchableOpacity>
-        </View>
+      <ScrollView contentContainerStyle = {styles.container}>
+        <Text style ={ styles.titulo}>Equipo: Chick and Duck</Text>
+        <Text style = {styles.texto}>García Hernandez Ana Gabriela</Text>
+        <Text style ={styles.texto}>Soria Muñoz Bibiana Paola</Text>
+        <Image style = {styles.imagen} source = {require('../../assets/IMG-20250509-WA0020.jpg')}/>
+      </ScrollView>
     )
 }
 
@@ -22,29 +22,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: "#26528F"
   },
   titulo: {
     fontSize: 20,
-    fontWeight: '600',
-    margin: 10,
-    backgroundColor: 'blue',
-    padding: 15,
     color: 'white',
-    textAlign: 'center'
-  },
-  boton: {
-    backgroundColor: "#e3e3e3", 
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 10,
-    width: 100,
-    height: 40,
-    marginTop: 10
-  },
-  texto_boton: {
-    fontSize: 14,
+    textAlign: 'center',
     fontWeight: '600',
-    color: 'black'
+    marginBottom: 10
+  },
+  texto:{
+    fontSize: 15,
+    marginBottom: 5,
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: '400'
+  },
+  imagen:{
+    height:180,
+    width: 198,
+    borderRadius: 35,
+    margin: 20
   }
+  
 });
