674de45dd25368c64caa86b973a3b6de334ef343
Bibiana Soria <bibiana_3141240181@utd.edu.mx>
2026-04-18 15:01:35 -0600

    creacion de la carpeta components

---
+++ src/components/component.js
@@ -0,0 +1,12 @@
+import react from "react";
+import {View, Text, StyleSheet } from 'react-native';
+
+export default function Component(){
+    return(
+        <View>
+            <Text>
+                Hola
+            </Text>
+        </View>
+    )
+}
